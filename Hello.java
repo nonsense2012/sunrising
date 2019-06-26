8b57d23fcaf4981f8d409e9f95197a18160c16dc
nonsense2012 <nonsense2012@sina.com>
2019-06-26 21:20:46 +0800

    url print

--- Hello.java
+++ Hello.java
@@ -1,6 +1,6 @@
 public class Hello{
 	public static void main(String args[]){
-		System.out.println("************");
-		System.out.println("############");
+		System.out.println("www.yootl.com");
+		System.out.println("www.mldnjava.cn");
 	}
 }
